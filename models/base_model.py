8513725b61bed6b444b5488cd038217bea64ff6a
“Liam-YorkRobertson” <liamyorkdeonrobertson@gmail.com>
2023-08-22 12:45:55 -0700

    changed __init__ func

--- models/base_model.py
+++ models/base_model.py
@@ -37,9 +37,9 @@ class BaseModel:
             if "id" not in kwargs:
                 self.id = str(uuid.uuid4())
             if "created_at" not in kwargs:
-                self.created_at = datetime.now()
+                self.created_at = datetime.utcnow()
             if "updated_at" not in kwargs:
-                self.updated_at = datetime.now()
+                self.updated_at = datetime.utcnow()
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
