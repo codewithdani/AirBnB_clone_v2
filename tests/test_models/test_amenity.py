10cb790e63e0d7bc75d3a55977205a0a29bba35d
Liam-YorkRobertson <liamyorkdeonrobertson@gmail.com>
2023-08-23 18:25:18 +0000

    update

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -12,10 +12,12 @@ class test_Amenity(test_basemodel):
     """
 
     def __init__(self, *args, **kwargs):
+        """ """
         super().__init__(*args, **kwargs)
         self.name = "Amenity"
         self.value = Amenity
 
     def test_name2(self):
+        """ """
         new = self.value()
         self.assertEqual(type(new.name), str)
