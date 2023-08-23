706f6aa42830f4a1ba1f9e70cd1a7c66160a0fb6
Liam-YorkRobertson <liamyorkdeonrobertson@gmail.com>
2023-08-23 18:54:35 +0000

    update

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -8,12 +8,13 @@ class test_state(test_basemodel):
     """ Class that tests the state.py """
 
     def __init__(self, *args, **kwargs):
-        """ Constructor for the test_state class """
         super().__init__(*args, **kwargs)
         self.name = "State"
         self.value = State
 
     def test_name3(self):
-        """ More testing of state name """
         new = self.value()
         self.assertEqual(type(new.name), str)
+
+if __name__ == "__main__":
+    unittest.main()
