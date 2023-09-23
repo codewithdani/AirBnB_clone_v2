f909e37b4bb082d34eb299765dc75955447b4b90
Daniel <daniel@gmail.com>
2023-09-23 17:11:54 +0300

    7-states_list.py

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" script that starts a Flask web application """
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def close_db(exc):
+    """close the current session of sqlalchemy"""
+    storage.close()
+
+
+@app.route('/states_list')
+def states_list():
+    """
+    Displays an HTML page with a list of all State objects in DBStorage.
+    States are sorted by name.
+    """
+    states = storage.all(State).values()
+    return render_template("7-states_list.html", states=states)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
